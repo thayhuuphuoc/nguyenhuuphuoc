589980c6580509f57d3317722f28abb8ffb99994
Nguyễn Hữu Phước <nhphuoc103@gmail.com>
2025-11-23 22:08:29 +0700

    Delete schemaTypes directory

--- schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import blockContent from "./blockContent"
-import category from "./category"
-import author from "./author"
-import post from "./post"
-
-export const schemaTypes = [post, author, category, blockContent]
